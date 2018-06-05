0f5b4dd83a1544af15d730424cff7e4192776e3d
Arunachalam <arunsha0007@gmail.com>
2018-06-04 18:32:18 -0700

    Backbone Module Inheritance

--- models/inheritancemain.js
+++ models/inheritancemain.js
@@ -6,6 +6,7 @@ var Animal = Backbone.Model.extend({
 
 var Dog = Animal.extend({
     walk: function () {
+        Animal.prototype.walk.apply(this); // this will call the animal call walk method
         console.log("Dog is walking...");
     }
 });
